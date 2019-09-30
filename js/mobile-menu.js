e91fc377f5dd21a28273027bafbe07cde9a425c4
Eduard Syasin <edward.syasin@gmail.com>
2019-09-30 14:24:38 +0300

    1.0
    Prod

---
+++ js/mobile-menu.js
@@ -0,0 +1,11 @@
+btn = document.getElementById('burger-btn');
+mm = document.getElementById('mm');
+
+najatine = function () {
+    mm.classList.add('mm-open');
+};
+
+
+nenajatine = function () {
+    mm.classList.remove('mm-open');
+};
